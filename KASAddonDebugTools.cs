9b7adea2f49d0251083cfc4aa8487629e1ddebb7
KospY <kospy@outlook.com>
2013-10-04 09:44:21 +0200

    Warning message if heightFromTerrain get negative (just in case)

--- KASAddonDebugTools.cs
+++ KASAddonDebugTools.cs
@@ -94,6 +94,11 @@ namespace KAS
                     }     
                 }
             }
+
+            if (FlightGlobals.ActiveVessel.heightFromTerrain == -1f)
+            {
+                KAS_Shared.DebugWarning("Warning ! ActiveVessel.heightFromTerrain are negative (" + FlightGlobals.ActiveVessel.heightFromTerrain + ") do not save now (vessel spawning underground bug) and please report this message to KospY on the KAS thread");
+            }
         }
 
         void OnGUI()
