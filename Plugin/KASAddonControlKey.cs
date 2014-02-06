c6467221d82eef71593a84790bf981b4b99ec91f
Alexander Gavrilov <angavrilov@gmail.com>
2014-02-06 20:45:51 +0400

    Add a check to avoid handling key presses when an edit field is active.
    This seems necessary to avoid making the numpad unusable on Linux.

--- Plugin/KASAddonControlKey.cs
+++ Plugin/KASAddonControlKey.cs
@@ -41,6 +41,12 @@ namespace KAS
 
         public void Update()
         {
+            // Ignore if an edit field is active
+            if (GUIUtility.keyboardControl != 0)
+            {
+                return;
+            }
+
             UpdateGrab();
             UpdateWinchMouseGrab();
             UpdateWinchKeyGrab();
