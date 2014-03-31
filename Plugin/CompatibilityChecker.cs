50afcc63dedcaa722fc649e8ebefa1cf6548f281
Majiir Paktu <majiir@nabaal.net>
2014-03-30 20:29:43 -0400

    Update compatible version

--- Plugin/CompatibilityChecker.cs
+++ Plugin/CompatibilityChecker.cs
@@ -54,7 +54,7 @@ namespace KAS
 
             const int compatibleMajor = 0;
             const int compatibleMinor = 23;
-            const int compatibleRevision = 0;
+            const int compatibleRevision = 5;
 
             return (Versioning.version_major == compatibleMajor) && (Versioning.version_minor == compatibleMinor) && (Versioning.Revision == compatibleRevision);
 
