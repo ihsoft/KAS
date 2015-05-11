64ffe1dcf2f586031a6698ccf1d1de1dd9960f4f
KospY <kospy@outlook.com>
2015-05-11 23:49:41 +0200

    Assembly update

--- Plugin/Properties/AssemblyInfo.cs
+++ Plugin/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("KAS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kerbal Systems")]
 [assembly: AssemblyProduct("KAS")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Kerbal Systems 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("0.4.7")]
+[assembly: AssemblyVersion("0.5.0")]
+[assembly: AssemblyInformationalVersion("0.5.0")]
+[assembly: KSPAssembly("KIS", 0, 5)]
