5f5cb13e93c70131687ee13cc02af1bfcee45287
ihsoft <igor.zavoychinskiy@gmail.com>
2016-06-24 23:15:56 -0700

    Cache rigidbody for better physics performance

--- Plugins/Source/KASModulePhysicChild.cs
+++ Plugins/Source/KASModulePhysicChild.cs
@@ -8,23 +8,22 @@ public class KASModulePhysicChild : PartModule {
   public float mass = 0.01f;
   public GameObject physicObj;
 
-  bool physicActive;
   Vector3 currentLocalPos;
   Quaternion currentLocalRot;
+  Rigidbody physicObjRb;
 
   /// <summary>Starts physics handling on the object.</summary>
   /// <remarks>The object is expected to not have Rigidbody. The one will be added with the proper
   /// mass and velocity settings.</remarks>
   public void StartPhysics() {
     KAS_Shared.DebugLog("StartPhysics(PhysicChild)");
-    if (!physicActive) {
-      var physicObjRigidbody = physicObj.AddComponent<Rigidbody>();
-      physicObjRigidbody.mass = mass;
+    if (physicObjRb == null) {
+      physicObjRb = physicObj.AddComponent<Rigidbody>();
       physicObj.transform.parent = null;
-      physicObjRigidbody.useGravity = false;
-      physicObjRigidbody.velocity = part.Rigidbody.velocity;
-      physicObjRigidbody.angularVelocity = part.Rigidbody.angularVelocity;
-      physicActive = true;
+      physicObjRb.mass = mass;
+      physicObjRb.useGravity = false;
+      physicObjRb.velocity = part.Rigidbody.velocity;
+      physicObjRb.angularVelocity = part.Rigidbody.angularVelocity;
     } else {
       KAS_Shared.DebugWarning("StartPhysics(PhysicChild) Physic already started! Ingore.");
     }
@@ -34,10 +33,10 @@ public class KASModulePhysicChild : PartModule {
   /// <remarks>Rigidbody on the object gets destroyed.</remarks>
   public void StopPhysics() {
     KAS_Shared.DebugLog("StopPhysics(PhysicChild)");
-    if (physicActive) {
-      UnityEngine.Object.Destroy(physicObj.GetComponent<Rigidbody>());
+    if (physicObjRb != null) {
+      UnityEngine.Object.Destroy(physicObjRb);
+      physicObjRb = null;
       physicObj.transform.parent = part.transform;
-      physicActive = false;
     } else {
       KAS_Shared.DebugWarning("StopPhysics(PhysicChild) Physic already stopped! Ignore.");
     }
@@ -46,11 +45,11 @@ public class KASModulePhysicChild : PartModule {
   /// <summary>Part's message handler.</summary>
   /// <remarks>Temporarily suspends physics handling on the object.</remarks>
   void OnPartPack() {
-    if (physicActive) {
+    if (physicObjRb != null) {
       KAS_Shared.DebugLog("OnPartPack(PhysicChild)");
       currentLocalPos = KAS_Shared.GetLocalPosFrom(physicObj.transform, part.transform);
       currentLocalRot = KAS_Shared.GetLocalRotFrom(physicObj.transform, part.transform);
-      physicObj.GetComponent<Rigidbody>().isKinematic = true;
+      physicObjRb.isKinematic = true;
       physicObj.transform.parent = part.transform;
       StartCoroutine(WaitPhysicUpdate());
     }
@@ -59,34 +58,28 @@ public class KASModulePhysicChild : PartModule {
   /// <summary>Part's message handler.</summary>
   /// <remarks>Resumes physics handling on the object.</remarks>
   void OnPartUnpack() {
-    if (physicActive) {
-      var physicObjRigidbody = physicObj.GetComponent<Rigidbody>();
-      if (physicObjRigidbody.isKinematic) {
-        KAS_Shared.DebugLog("OnPartUnpack(PhysicChild)");
-        physicObj.transform.parent = null;
-        KAS_Shared.SetPartLocalPosRotFrom(
-            physicObj.transform, part.transform, currentLocalPos, currentLocalRot);
-        physicObjRigidbody.isKinematic = false;
-        StartCoroutine(WaitPhysicUpdate());
-      }
+    if (physicObjRb != null && physicObjRb.isKinematic) {
+      KAS_Shared.DebugLog("OnPartUnpack(PhysicChild)");
+      physicObj.transform.parent = null;
+      KAS_Shared.SetPartLocalPosRotFrom(
+          physicObj.transform, part.transform, currentLocalPos, currentLocalRot);
+      physicObjRb.isKinematic = false;
+      StartCoroutine(WaitPhysicUpdate());
     }
   }
 
   /// <summary>Overriden from MonoBehavior.</summary>
   void OnDestroy() {
     KAS_Shared.DebugLog("OnDestroy(PhysicChild)");
-    if (physicActive) {
+    if (physicObjRb != null) {
       StopPhysics();
     }
   }
 
   /// <summary>Overriden from MonoBehavior.</summary>
   void FixedUpdate() {
-    if (physicActive) {
-      var rb = physicObj.GetComponent<Rigidbody>();
-      if (rb != null && !rb.isKinematic) {
-        rb.AddForce(part.vessel.graviticAcceleration, ForceMode.Acceleration);
-      }
+    if (physicObjRb != null && !physicObjRb.isKinematic) {
+      physicObjRb.AddForce(part.vessel.graviticAcceleration, ForceMode.Acceleration);
     }
   }
 
@@ -98,13 +91,12 @@ public class KASModulePhysicChild : PartModule {
     yield return new WaitForFixedUpdate();
     KAS_Shared.SetPartLocalPosRotFrom(
         physicObj.transform, part.transform, currentLocalPos, currentLocalRot);
-    var physicObjRigidbody = physicObj.GetComponent<Rigidbody>();
-    if (!physicObjRigidbody.isKinematic) {
+    if (!physicObjRb.isKinematic) {
       KAS_Shared.DebugLog(
           "WaitPhysicUpdate(PhysicChild) Set velocity to: {0} | angular velocity: {1}",
           part.Rigidbody.velocity, part.Rigidbody.angularVelocity);
-      physicObjRigidbody.angularVelocity = part.Rigidbody.angularVelocity;
-      physicObjRigidbody.velocity = part.Rigidbody.velocity;
+      physicObjRb.angularVelocity = part.Rigidbody.angularVelocity;
+      physicObjRb.velocity = part.Rigidbody.velocity;
     }
   }
 }
