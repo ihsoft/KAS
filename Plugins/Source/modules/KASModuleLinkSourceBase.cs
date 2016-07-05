1ae211e27c1e3d9c0aef651cfcea54cc50ad2570
ihsoft <igor.zavoychinskiy@gmail.com>
2016-07-05 01:46:15 -0700

    Base link modules

---
+++ Plugins/Source/modules/KASModuleLinkSourceBase.cs
@@ -0,0 +1,299 @@
+﻿// Kerbal Attachment System
+// Mod's author: KospY (http://forum.kerbalspaceprogram.com/index.php?/profile/33868-kospy/)
+// Module author: igor.zavoychinskiy@gmail.com
+// License: https://github.com/KospY/KAS/blob/master/LICENSE.md
+
+using System;
+using System.Linq;
+using UnityEngine;
+using KAS_API;
+using KSPDev.Processing;
+
+namespace KAS {
+
+/// <summary>Base link source module. Does all the job on making two parts linked.</summary>
+/// <remarks>This module deals with main logic of linking two parts together. The other party of the
+/// link must be aware of the linking porcess. The targets must implement <see cref="ILinkTarget"/>.
+/// <para>External callers must access methods and properties declared in base classes or interfaces
+/// only. Members and methods that are not part of these declarations are not intended for the
+/// public use <b>regardless</b> to their visibility level.</para>
+/// <para>Decendand classes may use any members and methods but good practice is restricting the
+/// usage to the interfaces and virtuals only.</para>
+/// </remarks>
+public class KASModuleLinkSourceBase : PartModule, ILinkSource, ILinkEventListener {
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkSource"/>.</para>
+  public string linkType { get { return type; } }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkSource"/>.</para>
+  public ILinkTarget linkTarget { get; private set; }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkSource"/>.</para>
+  public LinkState linkState {
+    get { return linkStateMachine.currentState; }
+    protected set {
+      var oldState = linkStateMachine.currentState;
+      linkStateMachine.currentState = value;
+      OnStateChange(oldState);
+    }
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkSource"/>.</para>
+  public virtual bool isLocked {
+    get { return linkState == LinkState.Locked; }
+    set { linkState = value ? LinkState.Locked : LinkState.Available; }
+  }
+
+  /// <summary>State machine that controls event reaction in different states.</summary>
+  /// <remarks>Primary usage of the machine is managing subscriptions to the different game events. 
+  /// It's highly discouraged to use it for firing events or taking actions. Initial state can be
+  /// setup under different circumstances, and the associated events and actions may get triggered
+  /// at the inappropriate moment.</remarks>
+  protected SimpleStateMachine<LinkState> linkStateMachine;
+
+  /// <summary>
+  /// 
+  /// </summary>
+  [KSPField(isPersistant = true)]
+  protected string tgtStrutPartID = "";
+  /// <summary>
+  /// 
+  /// </summary>
+  [KSPField(isPersistant = true)]
+  protected string tgtStrutVesselID = "";
+
+  /// <summary>
+  /// 
+  /// </summary>
+  [KSPField]
+  protected string type = string.Empty;
+
+  /// <summary>Initializes the object.</summary>
+  /// <remarks>Defines link state tranistion matrix.
+  /// <para>Overridden from <see cref="PartModule"/>.</para>
+  /// </remarks>
+  public override void OnAwake() {
+    linkStateMachine = new SimpleStateMachine<LinkState>(true /* strict */);
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.Available,
+        new[] {LinkState.Linking, LinkState.RejectingLinks});
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.Linking,
+        new[] {LinkState.Available, LinkState.Linked});
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.Linked,
+        new[] {LinkState.Available});
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.Locked,
+        new[] {LinkState.Available});
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.RejectingLinks,
+        new[] {LinkState.Available, LinkState.Locked});
+
+    linkStateMachine.AddStateHandlers(
+        LinkState.Available,
+        enterHandler: x => KASEvents.OnStartLinking.Add(OnStartLinking),
+        leaveHandler: x => KASEvents.OnStartLinking.Remove(OnStartLinking));
+    linkStateMachine.AddStateHandlers(
+        LinkState.RejectingLinks,
+        enterHandler: x => KASEvents.OnStopLinking.Add(OnStopLinking),
+        leaveHandler: x => KASEvents.OnStopLinking.Remove(OnStopLinking));
+    linkStateMachine.AddStateHandlers(
+        LinkState.Linking,
+        enterHandler: x => KASEvents.OnLinkAccepted.Add(OnLinkActionAccepted),
+        leaveHandler: x => KASEvents.OnLinkAccepted.Remove(OnLinkActionAccepted));
+
+    //FIXME
+    linkStateMachine.OnDebugStateChange = (from, to) =>
+        Debug.LogWarningFormat("SOURCE: Part {0} (id={1}), module {2}: {3}=>{4}",
+                               part.name, part.craftID, moduleName, from, to);
+  }
+
+  /// <summary>Destroys the object.</summary>
+  /// <remarks>Overridden from <see cref="MonoBehaviour"/>.</remarks>
+  protected virtual void OnDestroy() {
+    linkStateMachine.Stop();
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkSource"/>.</para>
+  public virtual void StartLinking(GUILinkMode mode) {
+    linkState = LinkState.Linking;
+    KASEvents.OnStartLinking.Fire(this);
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkSource"/>.</para>
+  public virtual void CancelLinking() {
+    linkState = LinkState.Available;
+    KASEvents.OnStopLinking.Fire(this);
+  }
+
+  /// <summary>Creates a logical link between the parts.</summary>
+  /// <remarks>Callers should take care of the physical connection between the game
+  /// objects.</remarks>
+  /// <para>Implements <see cref="ILinkSource"/>.</para>
+  public virtual bool LinkToTarget(ILinkTarget target) {
+    //FIXME
+    Debug.LogWarningFormat("LinkTargetToTarget: {0}", target);
+    if (!CheckCanLinkTo(target)) {
+      return false;
+    }
+    ConnectParts(target);
+    LinkParts(target);
+    return true;
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkSource"/>.</para>
+  public virtual void BreakCurrentLink() {
+    //FIXME
+    Debug.LogWarning("BreakCurrentLink");
+    if (linkState != LinkState.Linked) {
+      Debug.LogWarningFormat(
+          "Cannot break connection: part {0} is not connected to anything", part.name);
+      return;
+    }
+    //FIXME: implement decoupling
+    UnlinkParts();
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkSource"/>.</para>
+  public virtual bool CheckCanLinkTo(ILinkTarget linkTarget,
+                                     bool reportToGUI = false, bool reportToLog = true) {
+    string errorMsg = null;
+    if (part == linkTarget.part) {
+      errorMsg = "Cannot link part to itself";
+    } else if (linkType != linkTarget.linkType) {
+      errorMsg = "Incompatible target link type";
+    } else if (!linkStateMachine.CheckCanSwitchTo(LinkState.Linked)) {
+      errorMsg = "Source is not available for link";
+    } else if (linkTarget.linkState != LinkState.AcceptingLinks) {
+      errorMsg = "Target doesn't accept links";
+    }
+    if (errorMsg != null) {
+      if (reportToGUI || reportToLog) {
+        Debug.LogWarningFormat("Cannot link {0} (type={1}) and {2} (type={3}): {4}",
+                               part.name, linkType,
+                               linkTarget.part.name, linkTarget.linkType,
+                               errorMsg);
+      }
+      if (reportToGUI) {
+        ScreenMessages.PostScreenMessage(errorMsg, 5f, ScreenMessageStyle.UPPER_CENTER);
+      }
+      return false;
+    }
+    return true;
+  }
+
+  /// <summary>Initalizes moulde state on part start.</summary>
+  /// <remarks>Overridden from <see cref="PartModule"/>.</remarks>
+  public override void OnLoad(ConfigNode node) {
+    Debug.LogWarning("*** SOURCE: ON LOAD");
+  }
+
+  /// <summary>Initalizes module state on scene start.</summary>
+  /// <remarks>Overridden from <see cref="PartModule"/>.</remarks>
+  public override void OnStart(PartModule.StartState state) {
+    Debug.LogWarning("*** SOURCE: ON START");
+    linkStateMachine.Start(LinkState.Available);
+    linkState = linkState;  // Trigger state updates.
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkEventListener"/></para>
+  public virtual void OnJointBreak(float breakForce) {
+    Debug.LogWarningFormat("Connection between {0} and {1} has broken with force {2}",
+                           part.name, linkTarget.part.name, breakForce);
+    UnlinkParts();
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkEventListener"/></para>
+  public virtual void OnLinkCreatedEvent(KASEvents.LinkInfo info) {
+    // Lock this source if another source on the part made the link.
+    if (!isLocked && !ReferenceEquals(info.source, this)) {
+      isLocked = true;
+    }
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkEventListener"/></para>
+  public virtual void OnLinkBrokenEvent(KASEvents.LinkInfo info) {
+    // Unlock this source if link with another source one the part has broke.
+    if (isLocked && !ReferenceEquals(info.source, this)) {
+      isLocked = false;
+    }
+  }
+
+  /// <summary>Triggers when state has being assigned with a value.</summary>
+  /// <remarks>This method triggers even when new state doesn't differ from the old one. When it's
+  /// important to catch the transition check for <paramref name="oldState"/>.</remarks>
+  /// <param name="oldState">State prior to the change.</param>
+  protected virtual void OnStateChange(LinkState oldState) {
+  }
+
+  /// <summary>Creates logical link between two KSP parts.</summary>
+  /// <param name="target">Provides the target part setup.</param>
+  void ConnectParts(ILinkTarget target) {
+    //FIXME: implement
+    Debug.LogWarning("ConnectParts");
+  }
+
+  /// <summary>Logically links source and target.</summary>
+  /// <remarks>No actual connection is created in the game.</remarks>
+  /// <param name="target">Target to link with.</param>
+  void LinkParts(ILinkTarget target) {
+    var linkInfo = new KASEvents.LinkInfo(this, target);
+    linkTarget = target;
+    linkTarget.linkSource = this;
+    linkState = LinkState.Linked;
+    KASEvents.OnLinkCreated.Fire(linkInfo);
+    SendMessage("OnLinkCreatedEvent", linkInfo, SendMessageOptions.DontRequireReceiver);
+  }
+
+  /// <summary>Logically unlinks source and the current target.</summary>
+  /// <remarks>No actual connection is destroyed in the game.</remarks>
+  void UnlinkParts() {
+    var linkInfo = new KASEvents.LinkInfo(this, linkTarget);
+    linkTarget.linkSource = null;
+    linkTarget = null;
+    linkState = LinkState.Available;
+    KASEvents.OnLinkBroken.Fire(linkInfo);
+    SendMessage("OnLinkBrokenEvent", linkInfo, SendMessageOptions.DontRequireReceiver);
+  }
+
+  /// <summary>Sets rejecting mode when some other source has started connection mode.</summary>
+  /// <remarks>Event handler for <see cref="KASEvents.OnStartLinking"/>.</remarks>
+  /// <param name="source">Source module that started connecting mode.</param>
+  void OnStartLinking(ILinkSource source) {
+    if (!ReferenceEquals(this, source)) {
+      linkState = LinkState.RejectingLinks;
+    }
+  }
+
+  /// <summary>Restores available mode when other's source connection mode is over.</summary>
+  /// <remarks>Event handler for <see cref="KASEvents.OnStopLinking"/>.</remarks>
+  /// <param name="source">Source module that started the mode.</param>
+  void OnStopLinking(ILinkSource source) {
+    if (!ReferenceEquals(this, source)) {
+      linkState = LinkState.Available;
+    }
+  }
+
+  /// <summary>Establishes a link if target has accepted connection from this source.</summary>
+  /// <remarks>Any problems that prevent from a successful creation will be logged to the user. The
+  /// accepting party must ensure the link can be done.</remarks>
+  /// <param name="target">Target that has accepted connetion.</param>
+  void OnLinkActionAccepted(ILinkTarget target) {
+    if (CheckCanLinkTo(target, reportToGUI: true)) {
+      LinkToTarget(target);
+    }
+  }
+}
+
+}  // namespace
---
+++ Plugins/Source/modules/KASModuleLinkTargetBase.cs
@@ -0,0 +1,192 @@
+﻿// Kerbal Attachment System
+// Mod's author: KospY (http://forum.kerbalspaceprogram.com/index.php?/profile/33868-kospy/)
+// Module author: igor.zavoychinskiy@gmail.com
+// License: https://github.com/KospY/KAS/blob/master/LICENSE.md
+
+using System;
+using System.Linq;
+using UnityEngine;
+using KAS_API;
+using KSPDev.Processing;
+
+namespace KAS {
+
+/// <summary>Base link target module. Only controls target link state.</summary>
+/// <remarks>This module only deals with logic part of the linking. It remembers the source and
+/// notifies other modules on the part about the link state. The actual work to make the link
+/// significant in the game engine must be done by the link source, an implementation of
+/// <see cref="ILinkSource"/>.
+/// <para>External callers must access methods and properties declared in base classes or interfaces
+/// only. Members and methods that are not part of these declarations are not intended for the
+/// public use <b>regardless</b> to their visibility level.</para>
+/// <para>Decendand classes may use any members and methods but good practice is restricting the
+/// usage to the interfaces and virtuals only.</para>
+/// </remarks>
+// TODO(ihsoft): Add code samples.
+public class KASModuleLinkTargetBase : PartModule, ILinkTarget, ILinkEventListener {
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkTarget"/>.</para>
+  public string linkType { get { return type; } }
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkTarget"/>.</para>
+  public virtual bool isLocked {
+    get { return linkState == LinkState.Locked; }
+    set { linkState = value ? LinkState.Locked : LinkState.Available; }
+  }
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkTarget"/>.</para>
+  public LinkState linkState {
+    get { return linkStateMachine.currentState; }
+    private set {
+      var oldState = linkStateMachine.currentState;
+      linkStateMachine.currentState = value;
+      OnStateChange(oldState);
+    }
+  }
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkTarget"/>.</para>
+  public virtual ILinkSource linkSource {
+    get { return _linkSource; }
+    set {
+      var oldSource = _linkSource;
+      _linkSource = value;
+      linkState = value != null ? LinkState.Linked : LinkState.Available;
+      TriggerSourceChangeEvents(oldSource);
+    }
+  }
+  ILinkSource _linkSource;
+
+  // Here go settings from the part config. They must be of at least "protected" visiblity level for
+  // the game to handle them properly.
+  [KSPField]
+  protected string type = string.Empty;
+
+  /// <summary>State machine that controls event reaction in different states.</summary>
+  /// <remarks>Primary usage of the machine is managing subscriptions to the different game events. 
+  /// It's highly discouraged to use it for firing events or taking actions. Initial state can be
+  /// setup under different circumstances, and the associated events and actions may get triggered
+  /// at the inappropriate moment.</remarks>
+  protected SimpleStateMachine<LinkState> linkStateMachine;
+
+  /// <summary>Initalizes moulde state on part start.</summary>
+  /// <remarks>Overridden from <see cref="PartModule"/>.</remarks>
+  public override void OnStart(PartModule.StartState state) {
+    Debug.LogWarning("*** TARGET: ON START");
+    linkStateMachine.Start(LinkState.Available);
+    linkState = linkState;  // Trigger updates.
+  }
+
+  /// <summary>Initalizes moulde state on part start.</summary>
+  /// <remarks>Overridden from <see cref="PartModule"/>.</remarks>
+  public override void OnLoad(ConfigNode node) {
+    Debug.LogWarning("*** TARGET: ON LOAD");
+  }
+
+  /// <summary>Initializes the object. An alternative to constructor.</summary>
+  /// <remarks>Overridden from <see cref="PartModule"/>.</remarks>
+  public override void OnAwake() {
+    linkStateMachine = new SimpleStateMachine<LinkState>(true /* strict */);
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.Available,
+        new[] {LinkState.AcceptingLinks, LinkState.RejectingLinks});
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.AcceptingLinks,
+        new[] {LinkState.Available, LinkState.Linked, LinkState.Locked});
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.Linked,
+        new[] {LinkState.Available});
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.Locked,
+        new[] {LinkState.Available});
+    linkStateMachine.SetTransitionConstraint(
+        LinkState.RejectingLinks,
+        new[] {LinkState.Available, LinkState.Locked});
+
+    linkStateMachine.AddStateHandlers(
+        LinkState.Available,
+        enterHandler: x => KASEvents.OnStartLinking.Add(OnStartConnecting),
+        leaveHandler: x => KASEvents.OnStartLinking.Remove(OnStartConnecting));
+    linkStateMachine.AddStateHandlers(
+        LinkState.AcceptingLinks,
+        enterHandler: x => KASEvents.OnStopLinking.Add(OnStopConnecting),
+        leaveHandler: x => KASEvents.OnStopLinking.Remove(OnStopConnecting));
+    linkStateMachine.AddStateHandlers(
+        LinkState.RejectingLinks,
+        enterHandler: x => KASEvents.OnStopLinking.Add(OnStopConnecting),
+        leaveHandler: x => KASEvents.OnStopLinking.Remove(OnStopConnecting));
+
+    //FIXME
+    linkStateMachine.OnDebugStateChange = (from, to) =>
+        Debug.LogWarningFormat("TARGET: Part {0} (id={1}), module {2}: {3}=>{4}",
+                               part.name, part.craftID, moduleName, from, to);
+  }
+
+  /// <summary>Destroys the object. An alternative to destructor.</summary>
+  /// <remarks>Overridden from <see cref="MonoBehaviour"/>.</remarks>
+  public virtual void OnDestroy() {
+    linkStateMachine.Stop();
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkEventListener"/></para>
+  public virtual void OnJointBreak(float breakForce) {
+    // Do nothing. The source will handle all the work.
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkEventListener"/></para>
+  public virtual void OnLinkCreatedEvent(KASEvents.LinkInfo info) {
+    // Lock this target if another target on the part has accepted the link.
+    if (!isLocked && !ReferenceEquals(info.target, this)) {
+      isLocked = true;
+    }
+  }
+
+  /// <inheritdoc/>
+  /// <para>Implements <see cref="ILinkEventListener"/></para>
+  public virtual void OnLinkBrokenEvent(KASEvents.LinkInfo info) {
+    // Unlock this yarget if link with another target on the part has broke.
+    if (isLocked && !ReferenceEquals(info.target, this)) {
+      isLocked = false;
+    }
+  }
+
+  /// <summary>Triggers when state has being assigned with a value.</summary>
+  /// <remarks>This method triggers even when new state doesn't differ from the old one. When it's
+  /// important to catch the transition check for <paramref name="oldState"/>.</remarks>
+  /// <param name="oldState">State prior to the change.</param>
+  protected virtual void OnStateChange(LinkState oldState) {
+  }
+
+  /// <summary>Reacts on source link mode change.</summary>
+  /// <remarks>KAS events listener.</remarks>
+  /// <param name="source"></param>
+  void OnStartConnecting(ILinkSource source) {
+    linkState = (part != source.part && linkType == source.linkType)
+        ? LinkState.AcceptingLinks
+        : LinkState.RejectingLinks;
+  }
+
+  /// <summary>Reacts on source link mode change.</summary>
+  /// <remarks>KAS events listener.</remarks>
+  /// <param name="connectionSource"></param>
+  void OnStopConnecting(ILinkSource connectionSource) {
+    linkState = LinkState.Available;
+  }
+
+  /// <summary>Triggesr link/unlink events when needed.</summary>
+  /// <param name="oldSource">Link source before the change.</param>
+  void TriggerSourceChangeEvents(ILinkSource oldSource) {
+    if (oldSource != _linkSource) {
+      if (_linkSource != null) {
+        var linkInfo = new KASEvents.LinkInfo(_linkSource, this);
+        SendMessage("OnLinkCreatedEvent", linkInfo, SendMessageOptions.DontRequireReceiver);
+      } else {
+        var linkInfo = new KASEvents.LinkInfo(oldSource, this);
+        SendMessage("OnLinkBrokenEvent", linkInfo, SendMessageOptions.DontRequireReceiver);
+      }
+    }
+  }
+}
+
+}  // namespace
