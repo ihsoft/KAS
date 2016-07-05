0f40cf679fa896eeccc375bf07f9d01b193d5f6d
ihsoft <igor.zavoychinskiy@gmail.com>
2016-07-05 01:48:24 -0700

    Temporary KSPDev methods

---
+++ Plugins/Source/modules/KSPDev.cs
@@ -0,0 +1,183 @@
+﻿// Kerbal Development tools.
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KSPDev.Extensions;
+
+namespace KSPDev.Extensions {
+
+public static class Dictionaries {
+  public static V SetDefault<K, V>(this Dictionary<K, V> dict, K key) where V : new() {
+    V value;
+    if (dict.ContainsKey(key)) {
+      value = dict[key];
+    } else {
+      value = new V();
+      dict.Add(key, value);
+    }
+    return value;
+  }
+}
+
+}  // namespace
+
+namespace KSPDev.Processing {
+
+public sealed class SimpleStateMachine<T> where T : struct, IConvertible {
+  public T currentState {
+    get { return _currentState; }
+    set { SetState(value); }
+  }
+  T _currentState;
+  
+  public bool isStarted { get; private set; }
+  public bool isStrict { get; private set; }
+
+  public delegate void OnChange(T state);
+
+  public delegate void OnDebugChange(T fromState, T toState);
+  public OnDebugChange OnDebugStateChange;
+
+  Dictionary<T, List<OnChange>> enterHandlers = new Dictionary<T, List<OnChange>>();
+  Dictionary<T, List<OnChange>> leaveHandlers = new Dictionary<T, List<OnChange>>();
+  Dictionary<T, T[]> transitionContstraints = new Dictionary<T, T[]>();
+
+  public SimpleStateMachine(bool strict) {
+    isStrict = strict;
+  }
+
+  public void Start(T startState) {
+    if (!isStarted) {
+      isStarted = true;
+      _currentState = startState;
+      if (OnDebugStateChange != null) {
+        OnDebugStateChange(_currentState, _currentState);
+      }
+      FireEnterState();
+    }
+  }
+
+  public void Stop() {
+    if (isStarted) {
+      if (OnDebugStateChange != null) {
+        OnDebugStateChange(_currentState, _currentState);
+      }
+      FireLeaveState();
+      isStarted = false;
+    }
+  }
+
+  public void SetTransitionConstraint(T fromState, T[] toStates) {
+    transitionContstraints.Remove(fromState);
+    transitionContstraints.Add(fromState, toStates);
+  }
+
+  public void ResetTransitionConstraint(T fromState) {
+    transitionContstraints.Remove(fromState);
+  }
+
+  public void ForceSetState(T newState) {
+    if (OnDebugStateChange != null) {
+      OnDebugStateChange(_currentState, newState);
+    }
+    if (isStarted) {
+      FireLeaveState();
+    }
+    _currentState = newState;
+    FireEnterState();
+  }
+
+  //FIXME: drop
+  public void AddEnterHandler(T state, OnChange handler) {
+    enterHandlers.SetDefault(state).Add(handler);
+  }
+
+  //FIXME: drop
+  public void AddLeaveHandler(T state, OnChange handler) {
+    leaveHandlers.SetDefault(state).Add(handler);
+  }
+
+  public void RemoveEnterHandler(T state, OnChange handler) {
+    if (enterHandlers.ContainsKey(state)) {
+      enterHandlers[state].Remove(handler);
+    }
+  }
+
+  public void RemoveLeaveHandler(T state, OnChange handler) {
+    if (leaveHandlers.ContainsKey(state)) {
+      leaveHandlers[state].Remove(handler);
+    }
+  }
+
+  public void AddStateHandlers(
+      T state, OnChange enterHandler = null, OnChange leaveHandler = null) {
+    if (enterHandler != null) {
+      enterHandlers.SetDefault(state).Add(enterHandler);
+    }
+    if (leaveHandler != null) {
+      leaveHandlers.SetDefault(state).Add(leaveHandler);
+    }
+  }
+
+  public bool CheckCanSwitchTo(T newState) {
+    if (isStrict) {
+      return transitionContstraints.ContainsKey(_currentState)
+          && transitionContstraints[_currentState].IndexOf(newState) != -1;
+    }
+    return !transitionContstraints.ContainsKey(_currentState)
+        || transitionContstraints[_currentState].IndexOf(newState) != -1;
+  }
+
+  void SetState(T newState) {
+    if (!isStarted) {
+      throw new InvalidOperationException(string.Format(
+          "State machine is not started. Transition to state {0} is impossible", newState));
+    }
+    if (!_currentState.Equals(newState)) {
+      if (!CheckCanSwitchTo(newState)) {
+        throw new InvalidOperationException(string.Format(
+            "Transition {0}=>{1} is not allowed", _currentState, newState));
+      }
+      ForceSetState(newState);
+    }
+  }
+
+  void FireLeaveState() {
+    if (leaveHandlers.ContainsKey(_currentState)) {
+      FireEvents(leaveHandlers[_currentState], _currentState);
+    }
+  }
+
+  void FireEnterState() {
+    if (enterHandlers.ContainsKey(_currentState)) {
+      FireEvents(enterHandlers[_currentState], _currentState);
+    }
+  }
+
+  void FireEvents(List<OnChange> events, T trigerringEvent) {
+    foreach (var @event in events) {
+      @event.Invoke(trigerringEvent);
+    }
+  }
+}
+
+}  // namespace
+
+namespace KSPDev.GUIUtils {
+
+public static class PartContextMenu {
+  public static void InvalidateContextMenu(Part part) {
+    var windows = GameObject.FindObjectsOfType(typeof(UIPartActionWindow))
+        .Cast<UIPartActionWindow>()
+        .Where(w => w.part == part);
+    foreach (var window in windows) {
+      window.displayDirty = true;
+    }
+  }
+}
+
+}  // namespace
