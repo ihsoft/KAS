df6aaaea92027d0d81536d274af446a3d74e4878
ihsoft <igor.zavoychinskiy@gmail.com>
2016-09-30 01:46:19 -0700

    Tiny comment change

--- Plugins/Source/api_v1/ILinkStateEventListener.cs
+++ Plugins/Source/api_v1/ILinkStateEventListener.cs
@@ -13,11 +13,31 @@ namespace KASAPIv1 {
 //TODO(ihsoft): Add code samples.
 public interface ILinkStateEventListener {
   /// <summary>Triggers when a source on the part has created a link.</summary>
+  /// <remarks>
+  /// This event triggers <b>after</b> the actual coupling has happen. Modules can expect the the
+  /// joints are setup but their physics may not kicked in yet.
+  /// </remarks>
   /// <param name="info">Source and target information about the link.</param>
   void OnKASLinkCreatedEvent(KASEvents.LinkEvent info);
 
   /// <summary>Triggers when a source on the part has broke the link.</summary>
+  /// <remarks>
+  /// This event comes at different link states depending on what has initiated the link break:
+  /// <list>
+  /// <item>
+  /// If link has been broken via KAS source (<see cref="ILinkSource.BreakCurrentLink"/>) then this
+  /// event is fired <b>before</b> the parts have actually decoupled.
+  /// </item>
+  /// <item>
+  /// If link has been broken externally, e.g. via physics or by invoking
+  /// <see cref="Part.decouple"/> method, then this event is fired <b>after</b> the parts have
+  /// decoupled.
+  /// </item>
+  /// </list>
+  /// </remarks>
   /// <param name="info">Source and target information about the link.</param>
+  /// <seealso href="https://kerbalspaceprogram.com/api/class_part.html#a397e83d33e3053648580246c5bfc6408">
+  /// KSP: Part.decouple</seealso>
   void OnKASLinkBrokenEvent(KASEvents.LinkEvent info);
 }
 
