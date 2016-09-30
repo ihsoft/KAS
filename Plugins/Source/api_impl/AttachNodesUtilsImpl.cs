fe364cb2a111e212648f0e936f3213aecb87e0c3
ihsoft <igor.zavoychinskiy@gmail.com>
2016-09-29 21:37:57 -0700

    Add create/drop AN to the KAS utils

--- Plugins/Source/api_impl/AttachNodesUtilsImpl.cs
+++ Plugins/Source/api_impl/AttachNodesUtilsImpl.cs
@@ -44,6 +44,43 @@ class AttachNodesUtilsImpl : KASAPIv1.IAttachNodesUtils {
     nodeTransform.localRotation = Quaternion.LookRotation(attachNode.orientation);
     return nodeTransform;
   }
+
+  /// <inheritdoc/>
+  public AttachNode CreateAttachNode(Part part, string nodeName, Transform nodeTransform) {
+    //FIXME
+    Debug.LogWarningFormat("** Create AN {0} for {1}", nodeName, part.name);
+    var attachNode = part.findAttachNode(nodeName);
+    if (attachNode != null) {
+      Debug.LogWarningFormat(
+          "Not creating attach node {0} for {1} - already exists", nodeName, part.name);
+    } else {
+      attachNode = new AttachNode(nodeName, nodeTransform, 0, AttachNodeMethod.FIXED_JOINT);
+      part.attachNodes.Add(attachNode);
+    }
+    attachNode.attachMethod = AttachNodeMethod.FIXED_JOINT;
+    attachNode.nodeType = AttachNode.NodeType.Stack;
+    attachNode.nodeTransform = nodeTransform;
+    attachNode.owner = part;
+    return attachNode;
+  }
+
+  /// <inheritdoc/>
+  public void DropAttachNode(Part part, string nodeName) {
+    //FIXME
+    Debug.LogWarningFormat("** Drop attach node {0} in {1}", nodeName, part.name);
+    var attachNode = part.findAttachNode(nodeName);
+    if (attachNode == null) {
+      Debug.LogWarningFormat(
+          "Not dropping attach node {0} on {1} - not found", nodeName, part.name);
+      return;
+    }
+    if (attachNode.attachedPart != null) {
+      Debug.LogWarningFormat(
+          "Attach node {0} on {1} is attached to {2} - decouple callbacks will be impacted",
+          nodeName, part.name, attachNode.attachedPart.name);
+    }
+    part.attachNodes.Remove(attachNode);
+  }
 }
 
 }  // namespace
--- Plugins/Source/api_v1/IAttachNodesUtils.cs
+++ Plugins/Source/api_v1/IAttachNodesUtils.cs
@@ -42,6 +42,44 @@ public interface IAttachNodesUtils {
   /// <param name="attachNode">Node to get/create transform for.</param>
   /// <returns>Transform that is a child of the owning part transform.</returns>
   Transform GetOrCreateNodeTransform(AttachNode attachNode);
+
+  /// <summary>Creates a new attach node on the part.</summary>
+  /// <remarks>
+  /// It's expected there is no node with the same name on the part already. If there is one then no
+  /// extra node will be created, and properties of the existing node will be updated (see below).
+  /// Though, it's an unexpected situation, so a warning record will be logged.
+  /// <para>
+  /// The node will have the following properties:
+  /// <list>
+  /// <item>Size is "small".</item>
+  /// <item>Attach method is <c>FIXED_JOINT</c>.</item>
+  /// <item>Node type is <c>Stack</c>.</item>
+  /// </list>
+  /// </para>
+  /// </remarks>
+  /// <param name="part">Part to create node for.</param>
+  /// <param name="nodeName">Name of the node to create.</param>
+  /// <param name="nodeTransform">Transform that specifies node position and orientation.</param>
+  /// <seealso href="https://kerbalspaceprogram.com/api/class_attach_node.html">
+  /// KSP: AttachNode</seealso>
+  /// <seealso href="https://kerbalspaceprogram.com/api/_attach_node_8cs.html#ad750801f509bb71dc93caffbca90ad3d">
+  /// KSP: AttachNodeMethod</seealso>
+  /// <seealso href="https://kerbalspaceprogram.com/api/class_attach_node.html#a96e7fbc9722efd10a0e225bb6a6778cc">
+  /// KSP: AttachNode.NodeType</seealso>
+  AttachNode CreateAttachNode(Part part, string nodeName, Transform nodeTransform);
+
+  /// <summary>Drops the attach node on the part.</summary>
+  /// <remarks>
+  /// Don't drop an connected node until the part is decoupled. Otherwise,
+  /// decouple callback (<see cref="IActivateOnDecouple"/>) won't be called on the part.
+  /// </remarks>
+  /// <param name="part">Part to drop node on.</param>
+  /// <param name="nodeName">Name of the node to drop.</param>
+  /// <seealso href="https://kerbalspaceprogram.com/api/class_attach_node.html">
+  /// KSP: AttachNode</seealso>
+  /// <seealso href="https://kerbalspaceprogram.com/api/interface_i_activate_on_decouple.html">
+  /// KSP: IActivateOnDecouple</seealso>
+  void DropAttachNode(Part part, string nodeName);
 }
 
 }  // namespace
