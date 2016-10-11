73bc874cf18338a9cbc5d89394b2867a576f5bea
ihsoft <igor.zavoychinskiy@gmail.com>
2016-10-11 00:59:53 -0700

    Tiny code optimization

--- Source/KSPDev/Extensions/Dictionaries.cs
+++ Source/KSPDev/Extensions/Dictionaries.cs
@@ -10,12 +10,11 @@ namespace KSPDev.Extensions {
 public static class Dictionaries {
   public static V SetDefault<K, V>(this Dictionary<K, V> dict, K key) where V : new() {
     V value;
-    if (dict.ContainsKey(key)) {
-      value = dict[key];
-    } else {
-      value = new V();
-      dict.Add(key, value);
+    if (dict.TryGetValue(key, out value)) {
+      return value;
     }
+    value = new V();
+    dict.Add(key, value);
     return value;
   }
 }
