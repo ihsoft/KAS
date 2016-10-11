fb3cd91d09a6c018e0bcc3f1617c4ff648920c09
ihsoft <igor.zavoychinskiy@gmail.com>
2016-10-10 20:42:44 -0700

    Deprecate unused util code

--- Source/api_impl/AttachNodesUtilsImpl.cs
+++ Source/api_impl/AttachNodesUtilsImpl.cs
@@ -12,42 +12,6 @@ namespace KASImpl {
 /// <summary>Implements KASAPIv1.IAttachNodesUtils.</summary>
 class AttachNodesUtilsImpl : KASAPIv1.IAttachNodesUtils {
   /// <inheritdoc/>
-  public AttachNode GetAttachNode(Part part, string attachNodeName) {
-    var attachNode = part.findAttachNode(attachNodeName);
-    if (attachNode != null && attachNode.owner == null) {
-      Debug.LogWarningFormat("Fix null owner on part {0}, attach node {1}",
-                             part.name, attachNodeName);
-      attachNode.owner = part;
-    }
-    return attachNode;
-  }
-    
-  /// <inheritdoc/>
-  public Transform GetOrCreateNodeTransform(Part part, string attachNodeName) {
-    var attachNode = GetAttachNode(part, attachNodeName);
-    if (attachNode == null) {
-      Debug.LogWarningFormat("Cannot find attach node {0} on part {1}", attachNodeName, part.name);
-      return part.transform;
-    }
-    return GetOrCreateNodeTransform(attachNode);
-  }
-
-  /// <inheritdoc/>
-  public Transform GetOrCreateNodeTransform(AttachNode attachNode) {
-    if (attachNode.nodeTransform != null) {
-      attachNode.nodeTransform.gameObject.DestroyGameObject();
-    }
-    var nodeTransform = new GameObject().transform;
-    attachNode.nodeTransform = nodeTransform;
-    //nodeTransform.parent = attachNode.owner.transform;
-    nodeTransform.parent = Hierarchy.GetPartModelTransform(attachNode.owner);
-    nodeTransform.localPosition = attachNode.position;
-    nodeTransform.localScale = Vector3.one;
-    nodeTransform.localRotation = Quaternion.LookRotation(attachNode.orientation);
-    return nodeTransform;
-  }
-
-  /// <inheritdoc/>
   public AttachNode CreateAttachNode(Part part, string nodeName, Transform nodeTransform) {
     var attachNode = part.findAttachNode(nodeName);
     if (attachNode != null) {
--- Source/api_v1/Utils/IAttachNodesUtils.cs
+++ Source/api_v1/Utils/IAttachNodesUtils.cs
@@ -11,38 +11,6 @@ namespace KASAPIv1 {
 
 /// <summary>Various methods to deal with part's attach nodes.</summary>
 public interface IAttachNodesUtils {
-  /// <summary>Returns attach node by name.</summary>
-  /// <remarks>
-  /// This method ensures that the found node is properly initialized. As of KSP 1.1.3.1289 owner
-  /// of the node can be <c>null</c>. The node returned by this methid is guaranteed to have all the
-  /// members set as expected.
-  /// </remarks>
-  /// <param name="part">Part to look nodes in.</param>
-  /// <param name="attachNodeName">name of the node.</param>
-  /// <returns>Attch node or <c>null</c> if nothing is found.</returns>
-  AttachNode GetAttachNode(Part part, string attachNodeName);
-
-  /// <summary>Returns attach node transform. Creates it if it doesn't exist.</summary>
-  /// <remarks>
-  /// Attach node transform is located at the attach node position and looks in the same direction.
-  /// The returned transform is a child of the owning part transform, so it could be used as an
-  /// anchor in relational 3D calculations.
-  /// </remarks>
-  /// <param name="part">Part to lookup node in.</param>
-  /// <param name="attachNodeName">Name of the node.</param>
-  /// <returns>Transform that is a child of the part's transform.</returns>
-  Transform GetOrCreateNodeTransform(Part part, string attachNodeName);
-
-  /// <summary>Returns attach node transform. Creates it if it doesn't exist.</summary>
-  /// <remarks>
-  /// Attach node transform is located at the attach node position and looks in the same direction.
-  /// The returned transform is a child of the owning part transform, so it could be used as an
-  /// anchor in relational 3D calculations.
-  /// </remarks>
-  /// <param name="attachNode">Node to get/create transform for.</param>
-  /// <returns>Transform that is a child of the owning part transform.</returns>
-  Transform GetOrCreateNodeTransform(AttachNode attachNode);
-
   /// <summary>Creates a new attach node on the part.</summary>
   /// <remarks>
   /// It's expected there is no node with the same name on the part already. If there is one then no
