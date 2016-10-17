0959ca48353a6a49b71f53398b0f9e5355d83481
ihsoft <igor.zavoychinskiy@gmail.com>
2016-10-16 20:14:58 -0700

    Minor style change

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -6,20 +6,20 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("KAS")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Kerbal Systems")]
-[assembly: AssemblyProduct ("KAS")]
-[assembly: AssemblyCopyright ("Copyright © Kerbal Systems 2016")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyTitle("KAS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kerbal Systems")]
+[assembly: AssemblyProduct("KAS")]
+[assembly: AssemblyCopyright("Copyright © Kerbal Systems 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
+[assembly: ComVisible(false)]
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("b72369a7-e4f6-4d9b-9b9d-1e2ff315ef14")]
+[assembly: Guid("b72369a7-e4f6-4d9b-9b9d-1e2ff315ef14")]
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.0")]
-[assembly: AssemblyInformationalVersion ("1.0.0-pre1 for KSP 1.2")]
-[assembly: KSPAssembly ("KAS", 0, 5)]
-[assembly: AssemblyFlags (AssemblyNameFlags.PublicKey)]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyInformationalVersion("0.7.0 (KAS 1.0 BETA) for KSP 1.2")]
+[assembly: KSPAssembly("KAS", 0, 7)]
+[assembly: AssemblyFlags(AssemblyNameFlags.PublicKey)]
