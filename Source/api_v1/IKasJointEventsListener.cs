df3a760f61187f6506394f73c35fd57ee50dd720
ihsoft <igor.zavoychinskiy@gmail.com>
2016-12-28 20:45:34 -0800

    Helper class to notify part module about joint breakage on the object

---
+++ Source/api_v1/IKasJointEventsListener.cs
@@ -0,0 +1,24 @@
+﻿// Kerbal Attachment System
+// Mod's author: KospY (http://forum.kerbalspaceprogram.com/index.php?/profile/33868-kospy/)
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System;
+using UnityEngine;
+
+namespace KAS {
+
+/// <summary>Interface that notifies listeners about joints breaking.</summary>
+/// <seealso cref="BrokenJointListener"/>
+public interface IKasJointEventsListener {
+  /// <summary>Triggers when connection is broken due to too strong force applied.</summary>
+  /// <remarks>
+  /// This event is expected to be called from Unity <c>OnJointBreak</c> callback. It means it will
+  /// come from fixed frame update thread. Not all operations can be done from this thread.
+  /// </remarks>
+  /// <param name="hostObj">Game object that owns the joint.</param>
+  /// <param name="breakForce">Actual force that has been applied.</param>
+  void OnKASJointBreak(GameObject hostObj, float breakForce);
+}
+
+}  // namespace
---
+++ Source/modules/BrokenJointListener.cs
@@ -0,0 +1,74 @@
+﻿// Kerbal Attachment System
+// Mod's author: KospY (http://forum.kerbalspaceprogram.com/index.php?/profile/33868-kospy/)
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using KSPDev.KSPInterfaces;
+using KSPDev.LogUtils;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace KAS {
+
+/// <summary>Helper class to detect joint breakage and deliver event to the host part.</summary>
+/// <remarks>
+/// Modules that implement <see cref="IKasJointEventsListener"/> will get notified about a joint
+/// broken on the specific game object. It allows distinguishing joints in a multi-joint setup since
+/// normally Unity doesn't tell which joint has broken if there were more than one on the object.
+/// </remarks>
+/// <example>
+/// Let's say a module needs to create an extra joint to something. If it was added to the part
+/// then there would be <i>two</i> joints on the game object. When any of them got broken it would
+/// be impossible to figure out which one exactly is destroyed. To overcome this limitation an extra
+/// game object with a kinematic <see cref="Rigidbody"/> can be used:
+/// <code><![CDATA[
+/// class MyJoint : PartModule, IKasJointEventsListener {
+///   public override void OnStart(StartState state) {
+///     base.OnStart(state);
+///     var jointObj = new GameObject("MyFakeRB");
+///
+///     // Attach new object to the part.
+///     jointObj.transform.parent = part.transform;
+///     jointObj.transform.localPosition = Vector3.zero;
+///     jointObj.transform.localRotation = Quaternion.identity;
+///     jointObj.transform.localScale = Vector3.one;
+///
+///     // Add active modules.
+///     jointObj.AddComponent<Rigidbody>().isKinematic = true;
+///     jointObj.AddComponent<FixedJoint>().connectedBody = GetTargetBody();
+///     jointObj.AddComponent<BrokenJointListener>().hostPart = part;
+///   }
+///
+///   Rigidbody GetTargetBody() {
+///     return null;  // Let's pretend it returns a real target object.
+///   }
+///
+///   /// <inheritdoc/>
+///   public virtual void OnKASJointBreak(GameObject hostObj, float breakForce) {
+///     // Check for hostObj when a specific joint needs to be identified.
+///     if (hostObj.name == "MyFakeRB") {
+///       Debug.LogWarningFormat("Joint on MyFakeRB is broken with force {0}", breakForce);
+///     }
+///     Destroy(hostObj);
+///   }
+/// }
+/// ]]></code>
+/// </example>
+public class BrokenJointListener : MonoBehaviour,
+    // Syntax sugar interfaces.
+    IJointEventsListener {
+
+  /// <summary>Part to send messages to.</summary>
+  public Part hostPart;
+
+  /// <inheritdoc/>
+  public void OnJointBreak(float breakForce) {
+    Debug.LogFormat("Joint on {0} is broken with force {1}. Notifying part {2}",
+                    gameObject, breakForce, DbgFormatter.PartId(hostPart));
+    hostPart.FindModulesImplementing<IKasJointEventsListener>()
+        .ForEach(x => x.OnKASJointBreak(gameObject, breakForce));
+  }
+}
+
+}  // namespace
