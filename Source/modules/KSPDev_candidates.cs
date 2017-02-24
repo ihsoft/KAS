e4fcf79e54ccd1e5e81c4191a217aa62b4ce5e33
ihsoft <igor.zavoychinskiy@gmail.com>
2017-02-24 00:10:26 -0800

    Missing KSPDev cndaidates module

---
+++ Source/modules/KSPDev_candidates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace KSPDev.LogUtils {
+
+/// <summary>
+/// Merge with KSPDev.LogUtils.DbgFormatter
+/// </summary>
+public static class DbgFormatter2 {
+  /// <summary>Returns string represenation of a vector with more precision.</summary>
+  /// <param name="vec">Vector to dump.</param>
+  /// <returns>String representation.</returns>
+  public static string Vector(Vector3 vec) {
+    return string.Format("({0:0.0###}, {1:0.0###}, {2:0.0###})", vec.x, vec.y, vec.z);
+  }
+}
+  
+}  // namespace
