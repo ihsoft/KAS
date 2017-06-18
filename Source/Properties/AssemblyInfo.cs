82ed52bc53dc1a8ee46fe2cb4acd9fbd480b08e1
ihsoft <igor.zavoychinskiy@gmail.com>
2017-06-18 01:31:14 -0700

    Use new versioning approach
    Only set the major an minor part, leave the build and revison to the
    compiler.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.4")]
-[assembly: AssemblyInformationalVersion("0.7.4 (KAS 1.0 BETA) for KSP 1.3")]
-[assembly: KSPAssembly("KAS", 0, 7)]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0 Beta6-pre for KSP 1.3")]
+[assembly: KSPAssembly("KAS", 1, 0)]
 [assembly: AssemblyFlags(AssemblyNameFlags.PublicKey)]
