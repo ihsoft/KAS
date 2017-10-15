bf9ceecfd961b66e1724a48f7caa682e6a7f1269
ihsoft <igor.zavoychinskiy@gmail.com>
2017-10-14 21:13:54 -0700

    Stop guessing links from the attach nodes
    Always use the part id from the link.

--- Source/api_impl/LinkUtilsImpl.cs
+++ Source/api_impl/LinkUtilsImpl.cs
@@ -14,17 +14,6 @@ namespace KASImpl {
 class LinkUtilsImpl : ILinkUtils {
   /// <inheritdoc/>
   public ILinkTarget FindLinkTargetFromSource(ILinkSource source) {
-    // Docked parts must be connected via the attach nodes.
-    // Use them to doublecheck if the link is OK.
-    if (source.cfgLinkMode == LinkMode.DockVessels) {
-      if (source != null && source.attachNode != null && source.attachNode.attachedPart != null) {
-        return source.attachNode.attachedPart.FindModulesImplementing<ILinkTarget>()
-            .FirstOrDefault(s => s.attachNode != null && s.attachNode.attachedPart == source.part);
-      }
-      return null;
-    }
-    // The non-docked parts are bound via part IDs.
-    // Usen them to resolve the source<=>target relation.
     if (source.linkTargetPartId > 0) {
       var targetPart = FlightGlobals.FindPartByID(source.linkTargetPartId);
       return targetPart.FindModulesImplementing<ILinkTarget>().FirstOrDefault(
@@ -35,12 +24,6 @@ class LinkUtilsImpl : ILinkUtils {
 
   /// <inheritdoc/>
   public ILinkSource FindLinkSourceFromTarget(ILinkTarget target) {
-    // Docked parts must be connected via attach nodes. Use them to doublecheck if the link is OK.
-    if (target != null && target.attachNode != null && target.attachNode.attachedPart != null) {
-      return target.attachNode.attachedPart.FindModulesImplementing<ILinkSource>()
-          .FirstOrDefault(s => s.attachNode != null && s.attachNode.attachedPart == target.part);
-    }
-    // Non-docked parts are bound via part IDs. Usen them to resolve source<=>target relation.
     if (target.linkSourcePartId > 0) {
       var sourcePart = FlightGlobals.FindPartByID(target.linkSourcePartId);
       return sourcePart.FindModulesImplementing<ILinkSource>().FirstOrDefault(
