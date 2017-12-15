4cf95f6af63b539aaee596993dae09d98c0f1b22
ihsoft <igor.zavoychinskiy@gmail.com>
2017-12-15 00:02:15 -0800

    Tiny logging change

--- Source/api_impl/AttachNodesUtilsImpl.cs
+++ Source/api_impl/AttachNodesUtilsImpl.cs
@@ -37,10 +37,11 @@ class AttachNodesUtilsImpl : KASAPIv1.IAttachNodesUtils {
   /// <inheritdoc/>
   public void AddNode(Part part, AttachNode attachNode) {
     if (part.attachNodes.IndexOf(attachNode) == -1) {
-      DebugEx.Fine("Adding node {0} on {1}", DumpAttachNode(attachNode), part);
+      DebugEx.Fine("Adding node {0} to {1}", DumpAttachNode(attachNode), part);
       if (attachNode.owner != part) {
-        DebugEx.Fine("Former owner of the attach node doesn't match the new one: old={0}, new={1}",
-                     attachNode.owner, part);
+        DebugEx.Warning(
+            "Former owner of the attach node doesn't match the new one: old={0}, new={1}",
+            attachNode.owner, part);
         attachNode.owner = part;
       }
       part.attachNodes.Add(attachNode);
