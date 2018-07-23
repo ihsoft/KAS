d279e60b97c57ae81b799f5819dd786398d373be
ihsoft <igor.zavoychinskiy@gmail.com>
2018-07-22 19:34:11 -0700

    Load common config explicitly
    Don't use persistence attributes due to the common config class is an
    API class.

--- Source/api_v1/CommonConfig.cs
+++ Source/api_v1/CommonConfig.cs
@@ -3,27 +3,19 @@
 // Module author: igor.zavoychinskiy@gmail.com
 // License: Public Domain
 
-using KSPDev.ConfigUtils;
-
 namespace KASAPIv1 {
 
 /// <summary>Container for the various global settings of the mod.</summary>
 //FIXME: Make it API compatible (via an interface).
-[PersistentFieldsFile("KAS-1.0/settings.cfg", "KASConfig")]
 public static class CommonConfig {
   /// <summary>URL of the sound for the impossible action.</summary>
-  [PersistentField("Sounds/bipWrong")]
-  public static string sndPathBipWrong = "KAS-1.0/Sounds/bipwrong";
+  public static string sndPathBipWrong = "";
 
   /// <summary>Keyboard key to trigger the drop connector event.</summary>
-  /// <include file="SpecialDocTags.xml" path="Tags/ConfigSetting/*"/>
-  [PersistentField("Winch/dropConnectorKey")]
-  public static string keyDropConnector = "Y";
+  public static string keyDropConnector = "";
 
   /// <summary>Keyboard key to trigger the pickup connector event.</summary>
-  /// <include file="SpecialDocTags.xml" path="Tags/ConfigSetting/*"/>
-  [PersistentField("Winch/pickupConnectorKey")]
-  public static string keyPickupConnector = "Y";
+  public static string keyPickupConnector = "";
 
   internal static void Load() {
     ConfigAccessor.ReadFieldsInType(typeof(CommonConfig), null);
