5a9c2a4fcd5c45af550b547aeb8ad163e167804a
ihsoft <igor.zavoychinskiy@gmail.com>
2018-11-28 21:48:06 -0800

    Tiny OOP change
    Do not expose the internal class

--- Source/modules/KASInternalBrokenJointListener.cs
+++ Source/modules/KASInternalBrokenJointListener.cs
@@ -27,7 +27,7 @@ namespace KAS {
 /// overcome this limitation, an extra game object and this component can be used:
 /// <code source="Examples/BrokenJointListener-Examples.cs" region="BrokenJointListenerExample"/>
 /// </example>
-public class KASInternalBrokenJointListener : MonoBehaviour,
+sealed class KASInternalBrokenJointListener : MonoBehaviour,
     // KSP syntax sugar interfaces.
     IJointEventsListener {
 
