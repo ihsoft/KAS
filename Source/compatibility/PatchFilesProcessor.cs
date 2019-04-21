473d4bad20552d60aebdb7053be69d2627b88c89
ihsoft <igor.zavoychinskiy@gmail.com>
2019-04-21 02:44:31 -0700

    Tiny style fix

--- Source/compatibility/PatchFilesProcessor.cs
+++ Source/compatibility/PatchFilesProcessor.cs
@@ -33,7 +33,7 @@ sealed class PatchFilesProcessor : UpgradeScript {
     List<ConfigNodePatch> patches;
     var hasMatches = false;
     if (partPatches.TryGetValue(partName, out patches)) {
-      for (var i = patches.Count -1; i >= 0; --i) {
+      for (var i = patches.Count - 1; i >= 0; --i) {
         var patch = patches[i];
         try {
           hasMatches |= PartNodePatcher.TestPatch(node, patch, loadContext);
