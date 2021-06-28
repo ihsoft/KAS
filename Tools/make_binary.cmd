89e4659d29f6a3d44fcd16a5853da082b1b34dab
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2021-06-27 21:23:35 -0700

    Update make_binary.cmd

--- Tools/make_binary.cmd
+++ Tools/make_binary.cmd
@@ -1,4 +1,4 @@
 @echo off
 REM Until new major version of C# is released the build number in the path will keep counting.
 REM KAS *requires* .NET compiler version 4.0 or higher. Don't get confused with KSP run-time requirement of 3.5.
-"C:\Program Files (x86)\MSBuild\14.0\bin\MSBuild.exe" ..\Source\KAS.csproj /t:Clean,Build /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\amd64\MSBuild.exe" ..\Source\KAS.csproj /t:Clean,Build /p:Configuration=Release
