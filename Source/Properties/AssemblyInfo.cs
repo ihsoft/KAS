bd24a87f4e4df9fd43030356f1eb86c873ad7c37
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2021-07-01 00:03:02 -0700

    Start v1.9

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion ("1.8.*")]
-[assembly: AssemblyInformationalVersion ("1.8 for KSP v1")]
-[assembly: KSPAssembly ("KAS", 1, 8)]
+[assembly: AssemblyVersion ("1.9.*")]
+[assembly: AssemblyInformationalVersion ("1.9 for KSP v1")]
+[assembly: KSPAssembly ("KAS", 1, 9)]
 [assembly: AssemblyFlags (AssemblyNameFlags.PublicKey)]
